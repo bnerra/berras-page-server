05b138ca0291c659c150cfd9b4fc76f4a361a81b
CORPORATE\njb1019 <nickolas.berra@daugherty.com>
2018-11-29 14:27:53 -0600

    Initial setup

---
+++ README.md
@@ -0,0 +1,4 @@
+A serverless node.js endpoint for berras-page.
+
+To deploy to AWS Lambda:
+'serverless deploy'
---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const puppeteer = require('puppeteer')
+const mustacheExpress = require('mustache-express')
+
+let app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.engine('html', mustacheExpress())
+app.set('view engine', 'html')
+
+// public route that anyone can access
+app.get('/hello', (req, res) => {
+  return res.json({
+    message: 'Hello world!'
+  })
+})
+
+app.get('/html', (req, res) => {
+  const templateData = {  }
+  res.render('template.html', templateData)
+})
+
+
+app.get('/pdf', (req, res) => {
+  (async () => {
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      // await page.goto('https://i2wd7xn5sc.execute-api.us-east-1.amazonaws.com/dev/html')
+      await page.goto('http://localhost:8081/html')
+      const buffer = await page.pdf({format: 'A4', })
+      res.type('application/pdf')
+      res.send(buffer)
+      browser.close();
+  })()
+})
+
+
+module.exports = app
