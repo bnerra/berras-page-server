24faa2e054cd26101a2b18dcfd4a6e5c5e05cfd7
CORPORATE\njb1019 <nickolas.berra@daugherty.com>
2018-11-30 15:20:26 -0600

    use pdfkit for pdf generation

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const puppeteer = require('puppeteer')
-const mustacheExpress = require('mustache-express')
+const fs = require('fs')
+const PDFDocument = require('pdfkit')
 
 let app = express()
 app.use(cors())
 app.use(bodyParser.json())
-app.engine('html', mustacheExpress())
-app.set('view engine', 'html')
 
 // public route that anyone can access
 app.get('/hello', (req, res) => {
@@ -18,24 +16,34 @@ app.get('/hello', (req, res) => {
 })
 
 app.get('/html', (req, res) => {
-  const templateData = {  }
-  res.render('template.html', templateData)
+  const src = fs.createReadStream('./template.html');
+  src.pipe(res);
 })
-
 
 app.get('/pdf', (req, res) => {
-  (async () => {
+  const doc = new PDFDocument();
+  res.contentType("application/pdf");
+  res.setHeader('Content-disposition', 'attachment; filename=testFile.pdf')
+  doc.pipe(res);
 
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      // await page.goto('https://i2wd7xn5sc.execute-api.us-east-1.amazonaws.com/dev/html')
-      await page.goto('http://localhost:8081/html')
-      const buffer = await page.pdf({format: 'A4', })
-      res.type('application/pdf')
-      res.send(buffer)
-      await browser.close();
-  })()
+   doc.moveTo(300, 75)
+  .lineTo(373, 301)
+  .lineTo(181, 161)
+  .lineTo(419, 161)
+  .lineTo(227, 301)
+  .fill('red', 'even-odd');
+
+  const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in...';  
+
+  doc.y = 320;
+  doc.fillColor('black')
+  doc.text(loremIpsum, {
+    paragraphGap: 10,
+    indent: 20,
+    align: 'justify',
+    columns: 2
+  });  
+  doc.end()
 })
-
 
 module.exports = app
