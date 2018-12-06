3a1b2b7ba47fd6436a390231d5ad319bbb9ba1a5
Nick <nberra90@gmail.com>
2018-12-06 12:07:52 -0600

    Settings for pdf download

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const fs = require('fs')
-const PDFDocument = require('pdfkit')
+
 
 let app = express()
 let router = require('./routers/s3.router');
@@ -10,5 +9,23 @@ let router = require('./routers/s3.router');
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/', router);
+// app.use(function (req, res, next) {
+
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', false);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 module.exports = app
--- controllers/s3.controllers.js
+++ controllers/s3.controllers.js
@@ -6,11 +6,32 @@ exports.doDownload = (req, res) => {
   const s3Client = s3.s3Client;
   const params = s3.downloadParams;
 
-  var filestream = s3Client.getObject(params)
-    .createReadStream()
-      .on('error', function(err){
-        res.status(500).json({error:"Error -> " + err});
-      });
+  // var filestream = s3Client.getObject(params,)
+  //   .createReadStream()
+  //     .on('error', function(err){
+  //       res.status(500).json({error:"Error -> " + err});
+  //     });
 
-  filestream.pipe(res);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', false); // If needed
+  res.setHeader('isBase64Encoded', true);
+  res.setHeader('Content-Type', 'application/pdf');
+
+s3Client.getObject({
+  Bucket: 'bnerra.com',
+  Key: 'NBerraResume.pdf'
+}, function(err, data) {
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    res.send(data.Body.toString('base64'));
+    // res.send(data.Body);
+  }
+});
+
+
+  // filestream.pipe(res);
 }
